9af1b1195f62091a3fc493bc557c35f55acf60cd
suganeshC <suganesh73@gmail.com>
2022-03-11 09:40:54 +0530

    Toaster service added

--- B18BBookKart/src/app/homepage/homepage.component.ts
+++ B18BBookKart/src/app/homepage/homepage.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ToasterService } from '../toaster.service';
 
 @Component({
   selector: 'app-homepage',
@@ -7,9 +8,30 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomepageComponent implements OnInit {
 
-  constructor() { }
+  constructor(private data : ToasterService) { }
 
   ngOnInit(): void {
   }
+  addCart(message: string, action: string){
+    this.data.addCart('One Item added to cart','close');
+  }
+  removeCart(message: string, action: string){
+    this.data.removeCart('Product removed from cart','close');
+  }
+  addWishlist(message: string, action: string){
+    this.data.addWishlist('Item added to your wishlist','close');
 
+  }
+  removeWishlist(message: string, action: string){
+    this.data.removeWishlist('Item removed from your wishlist','close');
+  }
+  placeOrder(message: string, action: string){
+    this.data.placeOrder('Order placed successfully','close');
+  }
+  removeOrder(message: string, action: string){
+    this.data.removeOrder('Product removed from order','close');
+  }
+  clearCart(message: string, action: string){
+    this.data.clearCart('Cart list Cleared','close');
+  }
 }

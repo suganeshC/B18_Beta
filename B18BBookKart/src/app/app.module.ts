e0e582a154e4cc72de584abd4f925fcca8f8789c
suganeshC <suganesh73@gmail.com>
2022-03-11 10:18:30 +0530

    changes added

--- B18BBookKart/src/app/app.module.ts
+++ B18BBookKart/src/app/app.module.ts
@@ -3,28 +3,37 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HomepageComponent } from './homepage/homepage.component';
+
 import { LoginComponent } from './login/login.component';
+import { HomepageComponent } from './homepage/homepage.component';
 import { ErrorpageComponent } from './errorpage/errorpage.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-
 import {MatButtonModule} from '@angular/material/button';
-
+import {MatButtonToggleModule} from '@angular/material/button-toggle';
 import {MatCardModule} from '@angular/material/card';
+import {MatSnackBarModule} from '@angular/material/snack-bar';
+import {MatTreeModule} from '@angular/material/tree';
+import {OverlayModule} from '@angular/cdk/overlay';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomepageComponent,
     LoginComponent,
-    ErrorpageComponent
+    HomepageComponent,
+    ErrorpageComponent,
+  
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
     MatButtonModule,
-     MatCardModule,
+    MatButtonToggleModule,
+    MatCardModule, 
+    MatSnackBarModule,
+    MatTreeModule,
+    OverlayModule,
+  
     
   
   ],
